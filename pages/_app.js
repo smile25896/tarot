09d180ff752b721b63311627a1fdc6cf3ff88c94
Cathy <smile25896@gmail.com>
2022-06-15 22:01:35 +0800

    修正：redux store問題

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,11 @@ import withRedux from "next-redux-wrapper";
 import { Provider } from "react-redux";
 import configureStore from "../utils/configureStore";
 
+const store = configureStore();
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={configureStore()}>
+    <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
